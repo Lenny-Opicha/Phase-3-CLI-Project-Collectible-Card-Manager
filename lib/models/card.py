93f6e8835f5b18beb7f6b64d4d0215ba884ec2c1
Lenny-Opicha <ochangoopicha@gmail.com>
2026-01-30 01:10:20 +0300

    Fix Card model constructor and list-cards ORM mapping

--- lib/models/card.py
+++ lib/models/card.py
@@ -2,24 +2,39 @@ from lib.db import CURSOR, CONN
 
 
 class Card:
-    def __init__(self, id, collection_id, name, rarity):
+    def __init__(self, id, collection_id, name, description, rarity):
         self.id = id
         self.collection_id = collection_id
         self.name = name
+        self.description = description
         self.rarity = rarity
 
     @classmethod
-    def create(cls, collection_id, name, rarity):
+    def create(cls, collection_id, name, description, rarity):
         CURSOR.execute(
-            "INSERT INTO cards (collection_id, name, rarity) VALUES (?, ?, ?)",
-            (collection_id, name, rarity)
+            """
+            INSERT INTO cards (collection_id, name, description, rarity)
+            VALUES (?, ?, ?, ?)
+            """,
+            (collection_id, name, description, rarity)
         )
         CONN.commit()
-        return cls.find_by_id(CURSOR.lastrowid)
+
+        return cls(
+            CURSOR.lastrowid,
+            collection_id,
+            name,
+            description,
+            rarity
+        )
 
     @classmethod
-    def get_all(cls):
-        rows = CURSOR.execute("SELECT * FROM cards").fetchall()
+    def get_by_collection(cls, collection_id):
+        rows = CURSOR.execute(
+            "SELECT * FROM cards WHERE collection_id = ?",
+            (collection_id,)
+        ).fetchall()
+
         return [cls(*row) for row in rows]
 
     @classmethod
@@ -28,17 +43,13 @@ class Card:
             "SELECT * FROM cards WHERE id = ?",
             (id,)
         ).fetchone()
+
         return cls(*row) if row else None
 
     @classmethod
-    def find_by_collection(cls, collection_id):
-        rows = CURSOR.execute(
-            "SELECT * FROM cards WHERE collection_id = ?",
-            (collection_id,)
-        ).fetchall()
-        return [cls(*row) for row in rows]
-
-    @classmethod
     def delete(cls, id):
-        CURSOR.execute("DELETE FROM cards WHERE id = ?", (id,))
+        CURSOR.execute(
+            "DELETE FROM cards WHERE id = ?",
+            (id,)
+        )
         CONN.commit()
